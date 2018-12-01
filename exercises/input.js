d81f0dcfc8a04fe9e6a0919aca48e5fa7706f25c
rojrun <runroj@gmail.com>
2018-11-30 18:52:11 -0800

    completed featuresets 1 and 2

--- exercises/input.js
+++ exercises/input.js
@@ -4,34 +4,43 @@ class Input{
 	//constructor takes in a a target input element
 	//should also construct variables for the range, the pattern, and the element that will hold the error message
 	//range min and max should default to null
-	constructor( ){
-
+	constructor( testInput ){
+		this.testInput = $(testInput);
+		this.pattern = undefined;
+		this.minRange = null;
+		this.maxRange = null;
+        this.errorMessage;
 	}
 	//setRange sets the minimum and maximum range, if necessary, for the input
 	//arguments : min (a number), and max (a number)
 	//returns: nothing
 	//purpose: sets the min and max values for the object
-	setRange(  ){
-
+	setRange( min, max ){
+		this.minRange = min;
+		this.maxRange = max;
 	}
 	//getRange gets the minimum and maximum range.
 	//arguments: nothing
 	//returns: an object with a property of min, and a property of max, containing the minimum and maximum numbers
 	getRange(){
-		
+		var rangeObj = {
+				min: this.minRange,
+				max: this.maxRange
+		}
+		return rangeObj;
 	}
 	//setPattern saves a regex pattern into the object
 	//arguments: pattern (a regular expression.  if you don't know what it is, you will learn it soon)
 	//returns: nothing
 	//saves the given pattern into the object
-	setPattern(  ){
-		
+	setPattern( pattern ){
+		this.pattern = pattern;
 	}
 	//getPattern returns the currently stored pattern of the input object
 	//arguments: nothing
 	//returns: the currently stored regex pattern
 	getPattern(){
-		
+		return this.pattern;
 	}
 	//test runs all current tests on the target input and returns an object with data about whether the input passed or not
 	//arguments: nothing
@@ -49,14 +58,45 @@ class Input{
 		also needs to test the range, but note that the range is null by default
 		if it is still null, no range has been set, so don't test it
 			if it is not null, then test the range */
-	test(){
+    test(){
 
-	}
+        var successObject={};
+
+        var inputVal = this.testInput.val();
+
+        if (this.maxRange !== null && this.minRange !== null){
+            if(inputVal >= this.maxRange || inputVal <= this.minRange){
+                successObject={
+                    result:false,
+                    error:'range'
+                }
+            } else if (!this.pattern.test(inputVal)) {
+                successObject={
+                    result:false,
+                    error:'pattern'
+                }
+            } else {
+                successObject={
+                    result:true
+                }
+            }
+        } else if (!this.pattern.test(inputVal)) {
+            successObject={
+                result:false,
+                error:'pattern'
+            }
+        } else {
+            successObject={
+                result:true
+            }
+        }
+        return successObject;
+    }
 	/*
 	showError: takes in a message, creates a dom element, and then positions that dom Element directly below the input
 	argument: message (a string)
 	returns: nothing
-	notes: should create an element and positio it directly beneath the input
+	notes: should create an element and position it directly beneath the input
 		you will need to get the position of the input  (https://www.w3schools.com/jquery/css_position.asp)
 		also find the height of the element (https://www.w3schools.com/jquery/css_height.asp)
 		make a new dom element
@@ -69,17 +109,28 @@ class Input{
 		MAKE SURE TO STORE the reference to the dom element in the object for later use!
 		Don't store the CSS selector, you made the element, store the direct dom object itself!
 		*/
-	showError(  ){
-
+	showError( message ){
+		var positionInput = this.testInput.position();
+        var heightInput = this.testInput.height();
+        var parentInput = this.testInput.parent();
+        this.errorMessage = $("<div>",{
+			css: {
+				top: positionInput.top + heightInput,
+				left: positionInput.left
+			},
+			"class": "inputError",
+            text: message
+		});
+		parentInput.append(this.errorMessage);
 	}
 	/*
 	hideError removes the error dom element from the DOM for the given input
 	arguments: none
 	returns: none
-	note: 
+	note:
 		removes the dom element in question (https://www.w3schools.com/jquery/html_remove.asp)
 		*/
 	hideError(){
-		
+		this.errorMessage.remove();
 	}
 }
